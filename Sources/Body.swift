5b6363adea5eb913f2fd64122e74565efa856ab0
Scott Byrns <contact@scottbyrns.com>
2016-04-10 12:04:43 -0600

    Using .never in place of -1

--- Sources/Body.swift
+++ Sources/Body.swift
@@ -33,7 +33,7 @@ extension Body {
         case .buffer(let data):
             return data
         case .receiver(let receiver):
-            let data = Drain(for: receiver, timingOut: -1).data
+            let data = Drain(for: receiver, timingOut: .never).data
             self = .buffer(data)
             return data
         case .sender(let sender):
@@ -90,14 +90,14 @@ extension Body {
         switch self {
         case .buffer(let data):
             let closure: (Stream throws -> Void) = { sender in
-                try sender.send(data, timingOut: -1)
+                try sender.send(data, timingOut: .never)
             }
             self = .sender(closure)
             return closure
         case .receiver(let receiver):
             let closure: (Stream throws -> Void) = { sender in
-                let data = Drain(for: receiver, timingOut: -1).data
-                try sender.send(data, timingOut: -1)
+                let data = Drain(for: receiver, timingOut: .never).data
+                try sender.send(data, timingOut: .never)
             }
             self = .sender(closure)
             return closure
