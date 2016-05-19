588a0a1074384fcbead5587c711045a022323ac4
Joannis Orlandos <joannis@orlandos.nl>
2016-05-19 23:08:23 +0200

    I pressed ctrl+z too often. Bad mistake!

--- Sources/Multipart.swift
+++ Sources/Multipart.swift
@@ -1,4 +1,4 @@
-public enum MultiPart: Node {
+public enum MultiPart {
     case files([(filename: String?, mediaType: (type: String, subtype: String)?, data: Data)])
     case file(filename: String?, mediaType: (type: String, subtype: String)?, data: Data)
     case input(String)
